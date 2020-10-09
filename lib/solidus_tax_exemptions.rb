915ef25e52ae3bb596d5668573201179a8e4e2b6
Jared Norman <jared@super.gd>
2020-10-09 09:11:07 -0700

    Initial commit

---
+++ lib/solidus_tax_exemptions.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'solidus_core'
+require 'solidus_support'
+
+require 'solidus_tax_exemptions/configuration'
+require 'solidus_tax_exemptions/version'
+require 'solidus_tax_exemptions/engine'
+
+module SolidusTaxExemptions
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
+end
